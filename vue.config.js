a418ab2cbd7ac7d8644981a0fd337723f24f2048
pasichDev <apasichnik9@gmail.com>
2024-04-12 20:41:42 +0300

    add line gp

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+/* GITHUB PAGES
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/noso-explorer/" : "/",
+};
+*/
